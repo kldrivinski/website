1a382b4e5b048fdcf684b793200ea65fad5d6045
kelsey <drivinski@gmail.com>
2021-07-09 12:49:11 -0400

    consolidated components

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -14,75 +14,344 @@ export class ProjectsComponent implements OnInit {
   @Input() item: string;
   @Output() newItemEvent = new EventEmitter<string>();
 
-  photoArray: imageUrl[] = [];
+  previews: imageUrl[] = [];
+  beyond: imageUrl[] = [];
+  beyondImg: string[] = [];
+
+  jt: imageUrl[] = [];
+  jtImg: string[] = [];
+
+  mic: imageUrl[] = [];
+  micImg: string[] = [];
+
+  people: imageUrl[] = [];
+  peopleImg: string[] = [];
+
+  brainworms: imageUrl[] = [];
+  brainwormsImg: string[] = [];
+
+  mackinac: imageUrl[] = [];
+  mackinacImg: string[] = [];
+
+  pool: imageUrl[] = [];
+  poolImg: string[] = [];
+
+  eve: imageUrl[] = [];
+  eveImg: string[] = [];
+
+  eloise: imageUrl[] = [];
+  eloiseImg: string[] = [];
+
+  road: imageUrl[] = [];
+  roadImg: string[] = [];
+
   placeholder = [];
   data = [];
   image;
   results = {};
   index: number;
 
+  imgArray: string[] = [];
+
+  slider_img;
+  i: number = 0;
+  slidevisible: boolean = false;
+  gridvisible: boolean = true;
+
   constructor(private router: Router, private projectService: ProjectService) { }
 
 
-
-  // photoChange(e): void {
-  //   this.projectService.getDesert()
-  //     .subscribe((res: any) => {
-  //       console.log(res);
-
-
-  //       setTimeout(() => {
-  //         this.photoArray.splice(0, this.photoArray.length)
-  //         res.photoset.photo.forEach(ph => {
-  //           this.photoArray.push({
-  //             id: ph.id,
-  //             server: ph.server,
-  //             secret: ph.secret
-  //           });
-  //         });
-  //       }, 1000);
-  //     });
-
-
-
-
-  // }
 
   setIndex(index) {
     this.index = index;
   }
 
+  toggleSlider() {
+    this.slidevisible = !this.slidevisible;
+    this.gridvisible = !this.gridvisible;
+    console.log(this.slidevisible);
+    console.log(this.gridvisible);
+  }
   nav(index): void {
     console.log(index);
     if (index == 0) {
-      console.log("clicked")
-      this.router.navigate(["p1"]);
+      this.slidevisible = !this.slidevisible;
+      this.gridvisible = !this.gridvisible;
+      this.imgArray = this.beyondImg;
     }
     if (index == 1) {
-      console.log("clicked");
-      this.router.navigate(["p2"]);
+      this.slidevisible = !this.slidevisible;
+      this.gridvisible = !this.gridvisible;
+      this.imgArray = this.jtImg;
+
     }
-    else {
-      console.log("not a page")
+    if (index == 2) {
+      this.slidevisible = !this.slidevisible;
+      this.gridvisible = !this.gridvisible;
+      this.imgArray = this.micImg;
+
+    }
+    if (index == 3) {
+      this.slidevisible = !this.slidevisible;
+      this.gridvisible = !this.gridvisible;
+      this.imgArray = this.peopleImg;
+
+    }
+    if (index == 4) {
+      this.slidevisible = !this.slidevisible;
+      this.gridvisible = !this.gridvisible;
+      this.imgArray = this.brainwormsImg;
+
+    }
+    if (index == 5) {
+      this.slidevisible = !this.slidevisible;
+      this.gridvisible = !this.gridvisible;
+      this.imgArray = this.mackinacImg;
+
+    }
+    if (index == 6) {
+      this.slidevisible = !this.slidevisible;
+      this.gridvisible = !this.gridvisible;
+      this.imgArray = this.poolImg;
+
+    }
+    if (index == 7) {
+      this.slidevisible = !this.slidevisible;
+      this.gridvisible = !this.gridvisible;
+      this.imgArray = this.eveImg;
+
+    }
+    if (index == 8) {
+      this.slidevisible = !this.slidevisible;
+      this.gridvisible = !this.gridvisible;
+      this.imgArray = this.eloiseImg;
+
+    }
+    if (index == 9) {
+      this.slidevisible = !this.slidevisible;
+      this.gridvisible = !this.gridvisible;
+      this.imgArray = this.roadImg;
+
     }
   }
+
+  prev() {
+    console.log(this.imgArray);
+    if (this.i <= 1) this.i = this.imgArray.length;
+    this.i--;
+    return this.setImg();
+  }
+
+  next() {
+    if (this.i >= this.imgArray.length - 1) this.i = -1;
+    this.i++;
+    return this.setImg();
+  }
+  setImg() {
+    return this.slider_img.setAttribute('src', `${this.imgArray[this.i]}`);
+  }
   ngOnInit() {
+    this.slider_img = document.querySelector('.slider-img');
 
     this.projectService.getPhotos()
       .subscribe((res: any) => {
         res.photoset.photo.forEach(ph => {
-          this.photoArray.push({
+          this.previews.push({
             id: ph.id,
             server: ph.server,
             secret: ph.secret
           });
         });
-        this.image = 'https://live.staticflickr.com/' + res.photoset.photo[1].server + '/' + res.photoset.photo[1].id + '_' + res.photoset.photo[1].secret + '.jpg';
+        this.image = 'https://live.staticflickr.com/' + res.photoset.photo[0].server + '/' + res.photoset.photo[0].id + '_' + res.photoset.photo[0].secret + '.jpg';
         console.log(this.image);
-        console.log(this.photoArray);
+        console.log(this.previews);
 
       }
       );
+
+
+    this.projectService.getBeyond()
+      .subscribe((res: any) => {
+        res.photoset.photo.forEach(ph => {
+          this.beyond.push({
+            id: ph.id,
+            server: ph.server,
+            secret: ph.secret
+          });
+        });
+        //this.image = 'https://live.staticflickr.com/' + res.photoset.photo[1].server + '/' + res.photoset.photo[1].id + '_' + res.photoset.photo[1].secret + '.jpg';
+
+        this.beyond.forEach((ph, index) => {
+          var img = 'https://live.staticflickr.com/' + ph.server + '/' + ph.id + '_' + ph.secret + '.jpg';
+          this.beyondImg.push(img);
+        });
+      }
+      );
+
+    this.projectService.getJt()
+      .subscribe((res: any) => {
+        res.photoset.photo.forEach(ph => {
+          this.jt.push({
+            id: ph.id,
+            server: ph.server,
+            secret: ph.secret
+          });
+        });
+        //this.image = 'https://live.staticflickr.com/' + res.photoset.photo[1].server + '/' + res.photoset.photo[1].id + '_' + res.photoset.photo[1].secret + '.jpg';
+
+        this.jt.forEach((ph, index) => {
+          var img = 'https://live.staticflickr.com/' + ph.server + '/' + ph.id + '_' + ph.secret + '.jpg';
+          this.jtImg.push(img);
+        });
+      }
+      );
+
+    this.projectService.getMic()
+      .subscribe((res: any) => {
+        console.log('mic res ' + res.photoset)
+        // res.photoset.photo.forEach(ph => {
+        //   this.mic.push({
+        //     id: ph.id,
+        //     server: ph.server,
+        //     secret: ph.secret
+        //   });
+        // });
+        //this.image = 'https://live.staticflickr.com/' + res.photoset.photo[1].server + '/' + res.photoset.photo[1].id + '_' + res.photoset.photo[1].secret + '.jpg';
+
+        this.mic.forEach((ph, index) => {
+          var img = 'https://live.staticflickr.com/' + ph.server + '/' + ph.id + '_' + ph.secret + '.jpg';
+          this.micImg.push(img);
+        });
+      }
+      );
+
+    this.projectService.getPeople()
+      .subscribe((res: any) => {
+        res.photoset.photo.forEach(ph => {
+          this.people.push({
+            id: ph.id,
+            server: ph.server,
+            secret: ph.secret
+          });
+        });
+        //this.image = 'https://live.staticflickr.com/' + res.photoset.photo[1].server + '/' + res.photoset.photo[1].id + '_' + res.photoset.photo[1].secret + '.jpg';
+
+        this.people.forEach((ph, index) => {
+          var img = 'https://live.staticflickr.com/' + ph.server + '/' + ph.id + '_' + ph.secret + '.jpg';
+          this.peopleImg.push(img);
+        });
+      }
+      );
+
+    this.projectService.getBrainworms()
+      .subscribe((res: any) => {
+        res.photoset.photo.forEach(ph => {
+          this.brainworms.push({
+            id: ph.id,
+            server: ph.server,
+            secret: ph.secret
+          });
+        });
+        //this.image = 'https://live.staticflickr.com/' + res.photoset.photo[1].server + '/' + res.photoset.photo[1].id + '_' + res.photoset.photo[1].secret + '.jpg';
+
+        this.brainworms.forEach((ph, index) => {
+          var img = 'https://live.staticflickr.com/' + ph.server + '/' + ph.id + '_' + ph.secret + '.jpg';
+          this.brainwormsImg.push(img);
+        });
+      }
+      );
+
+    this.projectService.getMackinac()
+      .subscribe((res: any) => {
+        res.photoset.photo.forEach(ph => {
+          this.mackinac.push({
+            id: ph.id,
+            server: ph.server,
+            secret: ph.secret
+          });
+        });
+        //this.image = 'https://live.staticflickr.com/' + res.photoset.photo[1].server + '/' + res.photoset.photo[1].id + '_' + res.photoset.photo[1].secret + '.jpg';
+
+        this.mackinac.forEach((ph, index) => {
+          var img = 'https://live.staticflickr.com/' + ph.server + '/' + ph.id + '_' + ph.secret + '.jpg';
+          this.mackinacImg.push(img);
+        });
+      }
+      );
+
+    this.projectService.getPool()
+      .subscribe((res: any) => {
+        res.photoset.photo.forEach(ph => {
+          this.pool.push({
+            id: ph.id,
+            server: ph.server,
+            secret: ph.secret
+          });
+        });
+        //this.image = 'https://live.staticflickr.com/' + res.photoset.photo[1].server + '/' + res.photoset.photo[1].id + '_' + res.photoset.photo[1].secret + '.jpg';
+
+        this.pool.forEach((ph, index) => {
+          var img = 'https://live.staticflickr.com/' + ph.server + '/' + ph.id + '_' + ph.secret + '.jpg';
+          this.poolImg.push(img);
+        });
+      }
+      );
+
+    this.projectService.getEve()
+      .subscribe((res: any) => {
+        res.photoset.photo.forEach(ph => {
+          this.eve.push({
+            id: ph.id,
+            server: ph.server,
+            secret: ph.secret
+          });
+        });
+
+        //this.image = 'https://live.staticflickr.com/' + res.photoset.photo[1].server + '/' + res.photoset.photo[1].id + '_' + res.photoset.photo[1].secret + '.jpg';
+
+        this.eve.forEach((ph, index) => {
+          var img = 'https://live.staticflickr.com/' + ph.server + '/' + ph.id + '_' + ph.secret + '.jpg';
+          this.eveImg.push(img);
+        });
+      }
+      );
+
+    this.projectService.getEloise()
+      .subscribe((res: any) => {
+        res.photoset.photo.forEach(ph => {
+          this.eloise.push({
+            id: ph.id,
+            server: ph.server,
+            secret: ph.secret
+          });
+        });
+        //this.image = 'https://live.staticflickr.com/' + res.photoset.photo[1].server + '/' + res.photoset.photo[1].id + '_' + res.photoset.photo[1].secret + '.jpg';
+
+        this.eloise.forEach((ph, index) => {
+          var img = 'https://live.staticflickr.com/' + ph.server + '/' + ph.id + '_' + ph.secret + '.jpg';
+          this.eloiseImg.push(img);
+        });
+      }
+      );
+
+    this.projectService.getRoad()
+      .subscribe((res: any) => {
+        res.photoset.photo.forEach(ph => {
+          this.road.push({
+            id: ph.id,
+            server: ph.server,
+            secret: ph.secret
+          });
+        });
+        //this.image = 'https://live.staticflickr.com/' + res.photoset.photo[1].server + '/' + res.photoset.photo[1].id + '_' + res.photoset.photo[1].secret + '.jpg';
+
+        this.road.forEach((ph, index) => {
+          var img = 'https://live.staticflickr.com/' + ph.server + '/' + ph.id + '_' + ph.secret + '.jpg';
+          this.roadImg.push(img);
+        });
+      }
+      );
   }
+
 
 }
--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -21,11 +21,43 @@ export class ProjectService {
     return this.http.get(this.imagesUrl);
   }
 
-  getDesert() {
-    return this.http.get(`https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${this.apiKey}&photoset_id=72157718167654902&format=json&JSONP_CALLBACK=?&nojsoncallback=1`);
+  getBeyond() {
+    return this.http.get(`https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${this.apiKey}&photoset_id=72157712745023826&format=json&JSONP_CALLBACK=?&nojsoncallback=1`);
   }
 
-  getFilm() {
-    return this.http.get(`https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${this.apiKey}&photoset_id=72157712745023826&format=json&JSONP_CALLBACK=?&nojsoncallback=1`);
+  getJt() {
+    return this.http.get(`https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${this.apiKey}&photoset_id=72157719515551054&format=json&JSONP_CALLBACK=?&nojsoncallback=1`);
+  }
+
+  getMic() {
+    return this.http.get(`https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${this.apiKey}&photoset_id=/72157719515551059&format=json&JSONP_CALLBACK=?&nojsoncallback=1`);
+  }
+
+  getPeople() {
+    return this.http.get(`https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${this.apiKey}&photoset_id=72157719515551069&format=json&JSONP_CALLBACK=?&nojsoncallback=1`);
+  }
+
+  getBrainworms() {
+    return this.http.get(`https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${this.apiKey}&photoset_id=72157719515551074&format=json&JSONP_CALLBACK=?&nojsoncallback=1`);
+  }
+
+  getMackinac() {
+    return this.http.get(`https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${this.apiKey}&photoset_id=72157719515551079&format=json&JSONP_CALLBACK=?&nojsoncallback=1`);
+  }
+
+  getPool() {
+    return this.http.get(`https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${this.apiKey}&photoset_id=72157719515551089&format=json&JSONP_CALLBACK=?&nojsoncallback=1`);
+  }
+
+  getEve() {
+    return this.http.get(`https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${this.apiKey}&photoset_id=72157719515551094&format=json&JSONP_CALLBACK=?&nojsoncallback=1`);
+  }
+
+  getEloise() {
+    return this.http.get(`https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${this.apiKey}&photoset_id=72157719515551099&format=json&JSONP_CALLBACK=?&nojsoncallback=1`);
+  }
+
+  getRoad() {
+    return this.http.get(`https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${this.apiKey}&photoset_id=51300299155&format=json&JSONP_CALLBACK=?&nojsoncallback=1`);
   }
 }
